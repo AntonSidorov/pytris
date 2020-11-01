bcb7cb1077ebaa1bf9977828e2e160a6764014f1
Anton Sidorov <Anton.Sidorov@gmail.com>
2020-11-01 19:04:57 +1100

    fix: rename package name

--- python/setup.py
+++ python/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rl-pytris", # Replace with your own username
+    name="pytris", # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AntonSidorov/pytris",
+    url="https://github.com/AntonSidorov/pytris/python",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
